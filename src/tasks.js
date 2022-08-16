9f40a662af65ef0dd7ad5906249b86e8b07099d0
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2022-08-16 13:03:42 -0500

    Lista de tareas

---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id:0,
+        title: "mi primer tarea",
+        description: "mi primer tarea"
+    },
+    {
+        id:1,
+        title: "mi 2da tarea",
+        description: "mi 2da tarea"
+    },
+    {
+        id:2,
+        title: "mi 3ra tarea",
+        description: "mi 3ra tarea"
+    }
+]
